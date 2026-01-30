8a82c7fdfbe71f6931bdc10aa24650ed11e2b6e1
Martha659 <martha.blackcloud@students.msswarriors.org>
2026-01-30 19:55:23 +0000

    33

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 class Person {
-    consturcto(firstName, lastName) {
+    consturctor(firstName, lastName) {
     this.firstName = firstName;
     this.lastName = lastName;
  }
 }
 
 const guests = [
-    new Person ("John", "Doe")
-    new Person ("Jane", "Smith")
+    new Person ("John", "Doe"),
+    new Person ("Jane", "Smith"),
     new Person ("Alex", "Johnson")
 ];
